cf8f63341afd3bebc2568f2600990c3981a3e2bb
0xKaepora <0xkaeporagaebora@proton.me>
2024-05-26 12:25:03 -0700

    include wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+return {
+    color_scheme = 'Poimandres',
+    font = wezterm.font('Berkeley Mono'),
+    font_size = 11.0,
+    enable_tab_bar = false,
+    window_decorations = 'RESIZE',
+}
