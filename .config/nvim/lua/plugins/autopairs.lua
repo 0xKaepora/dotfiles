64893729dcd09f7be3577b60475406848c64f7f0
0xKaepora <0xkaeporagaebora@proton.me>
2024-08-28 23:07:09 -0500

    add aero and cleanup nvim

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,9 +1,8 @@
 return {
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-		-- use opts = {} for passing setup options
-		-- this is equalent to setup({}) function
-	},
+	"windwp/nvim-autopairs",
+	enabled = false,
+	event = "InsertEnter",
+	config = true,
+	-- use opts = {} for passing setup options
+	-- this is equalent to setup({}) function
 }
